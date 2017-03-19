fd37cd1665228a050b0c79d442c3f3b8e3f2b1ed
Jason Harrop <jason@plutext.org>
2017-03-19 11:14:02 +1100

    font mapping example

--- src/samples/java/org/docx4j/samples/ConvertInXHTMLFragment.java
+++ src/samples/java/org/docx4j/samples/ConvertInXHTMLFragment.java
@@ -35,7 +35,9 @@ import java.util.Set;
 import org.apache.commons.io.FileUtils;
 import org.docx4j.XmlUtils;
 import org.docx4j.convert.in.xhtml.XHTMLImporterImpl;
+import org.docx4j.jaxb.Context;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.wml.RFonts;
 
 /**
  * This sample converts a fragment of XHTML to docx.  The fragment should
@@ -89,6 +91,12 @@ public class ConvertInXHTMLFragment {
 //		XHTMLImporter.setCssWhiteList(cssWhiteList);
 		
         XHTMLImporterImpl XHTMLImporter = new XHTMLImporterImpl(wordMLPackage);
+        
+        
+//        RFonts arialRFonts = Context.getWmlObjectFactory().createRFonts();
+//        arialRFonts.setAscii("Arial");
+//        arialRFonts.setHAnsi("Arial");
+//        XHTMLImporterImpl.addFontMapping("SomeFont", arialRFonts);        
 		
 		wordMLPackage.getMainDocumentPart().getContent().addAll( 
 				XHTMLImporter.convert( xhtml, null) );
