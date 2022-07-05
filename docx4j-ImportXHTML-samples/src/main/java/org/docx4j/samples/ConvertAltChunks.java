40c62feb3815c6026fe90669d3ad96ac7d574afc
Jason Harrop <jason@plutext.org>
2022-07-06 08:42:12 +1000

    also convert altChunks in headers/footers

--- docx4j-ImportXHTML-samples/src/main/java/org/docx4j/samples/ConvertAltChunks.java
+++ docx4j-ImportXHTML-samples/src/main/java/org/docx4j/samples/ConvertAltChunks.java
@@ -3,35 +3,42 @@ package org.docx4j.samples;
 import java.io.File;
 
 import org.docx4j.Docx4J;
-import org.docx4j.XmlUtils;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.WordprocessingML.AltChunkType;
-import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.openpackaging.parts.JaxbXmlPartAltChunkHost;
+import org.docx4j.openpackaging.parts.relationships.Namespaces;
+import org.docx4j.relationships.Relationship;
 
 /**
- * Open a docx containing an XHTML AltChunk,
+ * Open a docx containing an XHTML or .mht AltChunk,
  * and then convert that to normal docx content.
  * @author jharrop
  *
  */
 public class ConvertAltChunks {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)  throws Exception {
 
-		WordprocessingMLPackage wordMLPackage = Docx4J.load(new File(System.getProperty("user.dir") + "/your.docx"));
+		WordprocessingMLPackage wordMLPackage = Docx4J.load(new File(System.getProperty("user.dir") + "your.docx"));
 				
 				
-		// Round trip
-		WordprocessingMLPackage pkgOut = wordMLPackage.getMainDocumentPart().convertAltChunks();
+		// MDP
+		wordMLPackage.getMainDocumentPart().convertAltChunks();
 		
+		// Headers/footers
+	    for (Relationship rel : wordMLPackage.getMainDocumentPart().getRelationshipsPart().getRelationships().getRelationship() ) {
+	    	
+	    	if (rel.getType().equals(Namespaces.HEADER)
+	    			|| rel.getType().equals(Namespaces.FOOTER)) {
+	    		JaxbXmlPartAltChunkHost part = (JaxbXmlPartAltChunkHost)wordMLPackage.getMainDocumentPart().getRelationshipsPart().getPart(rel);
+	    		System.out.println("processing " + part.getPartName().getName());
+	    		part.convertAltChunks();
+	    	}
+	    }		
 		// Display result
-		System.out.println(
-				XmlUtils.marshaltoString(pkgOut.getMainDocumentPart().getJaxbElement(), true, true));
+//		System.out.println(
+//				XmlUtils.marshaltoString(pkgOut.getMainDocumentPart().getJaxbElement(), true, true));
 		
-		pkgOut.save(new File(System.getProperty("user.dir") + "/OUT_AltChunkXHTMLRoundTrip.docx"));
+	    wordMLPackage.save(new File(System.getProperty("user.dir") + "/OUT_ConvertAltChunks_all.docx"));
 		
 		
 	}
